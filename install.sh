8b760a4dd796487aac8f79087c8e14bcfc621c6a
aryan <47309918+winexe0@users.noreply.github.com>
2020-07-09 21:04:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ sudo apt-get install wget coreutils p7zip
 wget https://winworldpc.com/download/c3902122-6c25-c382-11c3-a6e280947e52/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
 p7zip -d c39ac2af-c381-c2bf-1b25-11c3a4e284a2
 rm c39ac2af-c381-c2bf-1b25-11c3a4e284a2
-
+wget https://raw.githubusercontent.com/winexe0/minivmac-on-a-raspberry-pi/master/B82DD72C-DC65-4673-8298-D59081DBBAF0.png
+sudo mv -p B82DD72C-DC65-4673-8298-D59081DBBAF0.png /usr/share/minivmac/
+wget https://raw.githubusercontent.com/winexe0/minivmac-on-a-raspberry-pi/master/minivmac.desktop
+sudo mv minivmac.desktop /usr/share/applications/
