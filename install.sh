b00322767b278f28542ed40bfb68972e9241ed91
aryan <47309918+winexe0@users.noreply.github.com>
2020-07-09 21:17:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install wget coreutils p7zip
+sudo apt-get install wget coreutils p7zip tar 
 wget https://winworldpc.com/download/c3902122-6c25-c382-11c3-a6e280947e52/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
 p7zip -d c39ac2af-c381-c2bf-1b25-11c3a4e284a2
 rm c39ac2af-c381-c2bf-1b25-11c3a4e284a2
@@ -8,3 +8,14 @@ wget https://raw.githubusercontent.com/winexe0/minivmac-on-a-raspberry-pi/master
 sudo mv -p B82DD72C-DC65-4673-8298-D59081DBBAF0.png /usr/share/minivmac/
 wget https://raw.githubusercontent.com/winexe0/minivmac-on-a-raspberry-pi/master/minivmac.desktop
 sudo mv minivmac.desktop /usr/share/applications/
+wget https://www.gryphel.com/d/minivmac/minivmac-36.04/mnvm0026-36.04-larm.bin.tgz
+tar -xzf mnvm0026-36.04-larm.bin.tgz
+mv Mini\ vMac\ 26 minivmac
+sudo mv ninivmac /usr/bin/
+echo What disksize do you want (In Megabytes)?
+read disksize
+echo What is the name of the disk you want?
+read name
+echo Creating a $disksize MB hard disk named $name.
+dd if=/dev/zero of=$name bs=1M count=$disksize
+
